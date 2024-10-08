ff264efbb78ef25a9955282cca34678efec23f2f
PjBierhals <pjbierhals@gmail.com>
2024-10-08 15:32:56 -0300

    componentes

---
+++ src/app/componente/basico/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return (<h1>exemplo1</h1>)
+}
---
+++ src/app/componente/titulo/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+        <div>
+
+            <h1 className="text-red-500 text-6xl font-black">Titulo</h1>
+            <h2 className="text-green-500 text-4xl ">SubTitulo</h2>
+        </div>
+    )
+}
